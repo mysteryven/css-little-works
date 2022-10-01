07a04d653e14e004b60265d461bd6d9ff8567db2
wenzhe <mysteryven@gmail.com>
2022-10-01 18:53:24 +0800

    refactor: css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
 import type { FC } from 'react'
 import React from 'react'
 import works from '../works'
-import { mainPanel, menuItem, menuItemName, panelGrid as panelGrid, panelTitle } from '../css/index.css'
+import '../css/index.css'
 
 const Main: FC = () => {
   return (
-    <div className={mainPanel}>
+    <div className="mainPanel">
       <div>
-        <div className={panelTitle}>CSS Animation</div>
-        <pre className={panelGrid}>
+        <div className="panelTitle">CSS Animation</div>
+        <pre className="panelGrid">
           {
             works.map((item, index) => {
               return (
-                <div key={item.name} className={menuItem}>
+                <div key={item.name} className="menuItem">
                   <span>{index.toString().padStart(3, '0')}</span>
-                  <b className={menuItemName}>{item.name}</b>
+                  <b className="menuItemName">{item.name}</b>
                 </div>
               )
             })
